303533227609fa0125d66b0e4b937f9482d0d74d
Jenny Wettlaufer <jennywettlaufer@JennysMacBookPro.ed.shawcable.net>
2020-04-16 18:44:46 -0600

    inital commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <li v-if="!todo.completed">
+      <input type="checkbox" v-on:click="completeTodo( todo )">
+      {{todo.task}}
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: [
+        'todo'
+    ],
+    methods: {
+        completeTodo (todo) {
+            this.$emit('complete-todo', todo);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
