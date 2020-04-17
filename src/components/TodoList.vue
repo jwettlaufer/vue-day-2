585ec72d7f5c17f7d657fa8ba3251e7e88e97613
Jenny Wettlaufer <jennywettlaufer@JennysMacBookPro.ed.shawcable.net>
2020-04-16 19:26:33 -0600

    added functionality for new todo item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,12 @@
 <template>
   <section>
     <h2>To-Do List</h2>
-    <form>
+    <form @submit="addTodo">
       <label for="new-todo">
         Add a New To-Do:
-        <input type="text" name="to-do" v-model="newTodo">
+        <input type="text" name="to-do" v-model="newTodo" />
       </label>
-      <input type="submit" value="Submit">
+      <input type="submit" value="Submit" />
     </form>
     <ul>
       <Todo v-for="todo in todos" :key="todo.task" :todo="todo" v-on:complete-todo="completeTodo" />
@@ -49,6 +49,15 @@ export default {
     completeTodo(todo) {
       const todoIndex = this.todos.indexOf(todo);
       this.todos[todoIndex].completed = true;
+    },
+    addTodo(event) {
+        event.preventDefault(); // Stop the form from refreshing the page!
+      const newTodo = this.newTodo;
+      this.todos.push({
+        task: newTodo,
+        completed: false
+      });
+      this.newTodo = ""; //Clear the value after adding, so it is easy for the user to add more.
     }
   }
 };
